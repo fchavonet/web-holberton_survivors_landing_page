f88e863fc8cc24e61010394f13407f6423931175
fchavonet <fchavonet@mailo.com>
2024-02-29 22:07:44 +0100

    Add an adaptive logo to the navigation bar.

--- behavior.js
+++ behavior.js
@@ -2,6 +2,8 @@
 
 ////////// SCROLLING ELEMENTS BEHAVIOR \\\\\\\\\\
 
+// Select the Holberton Survivors top logo
+const holbertonSurvivorsTopLogo = document.querySelector("#holberton_survivors_top_logo");
 // Select the move-to-top button
 const moveToTopButton = document.querySelector("#move_to_top_button");
 
@@ -10,10 +12,16 @@ window.addEventListener("scroll", () => {
   // Get the current scroll position
   let scrollPosition = window.scrollY || document.documentElement.scrollTop;
 
-  // Show or hide the button based on scroll position
+  // Show or hide the button and logo based on scroll position
   if (scrollPosition > 400) {
+    // Make the Holberton Survivors top logo visible
+    holbertonSurvivorsTopLogo.style.opacity = "1";
+    // Show the move-to-top button
     moveToTopButton.style.bottom = "25px";
   } else {
+    // Hide the Holberton Survivors top logo
+    holbertonSurvivorsTopLogo.style.opacity = "0";
+    // Hide the move-to-top button
     moveToTopButton.style.bottom = "-50px";
   }
 });
